dd723804d4c67348f0e744fb84c5d78075b0f48d
stefian <v_ianta@yahoo.com>
2019-05-28 09:58:56 -0400

    added body-parser and createPost route and controller

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+// app.use(bodyParser.urlencoded());  // x-www-form-urlencoded in a <form> POST request
+app.use(bodyParser.json()); // for application/json
 
 app.use('/feed', feedRoutes);
 
