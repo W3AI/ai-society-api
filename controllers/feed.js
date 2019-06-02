fef6f3ee84a75f73d52218eed499bdeab1bc13f5
stefian <v_ianta@yahoo.com>
2019-06-02 18:43:01 -0400

    added validation logic for title and content and response to client if errors

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+// const fs = require('fs');
+const { validationResult } = require('express-validator/check');
 
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
@@ -17,6 +18,15 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+        return res
+            .status(422)
+            .json({
+                message: 'Validation failed, entered data is incorrect.',
+                errors: errors.array()
+            });
+    }
     const title = req.body.title;
     const content = req.body.content;
     // create post in db
@@ -24,8 +34,8 @@ exports.createPost = (req, res, next) => {
         message: 'Post created successfully!',
         post: {
             _id: new Date().toISOString(),
-            title: title, 
-            content: content, 
+            title: title,
+            content: content,
             creator: { name: 'Steve' },
             createdAt: new Date()
         }
