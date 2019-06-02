b5e39c7ab9d95ed31fe40aa9bd20e6d9c6b66337
stefian <v_ianta@yahoo.com>
2019-06-02 16:22:56 -0400

    added _id, creator and date for First Post

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,9 +4,14 @@ exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
             {
+                _id: '1',
                 title: 'First Post', 
                 content: 'This is the first post!', 
-                imageUrl: 'images/duck.jpeg'
+                imageUrl: 'images/duck.jpeg',
+                creator: {
+                    name: 'Steve'
+                },
+                date: new Date()
             }]
     });
 };
