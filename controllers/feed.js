3423ff0f0458c04933968db85f59bdaccb5a55c8
stefian <v_ianta@yahoo.com>
2019-06-02 20:29:32 -0400

    changed createPost in controllers/feed.js to save in db

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,8 @@
 // const fs = require('fs');
 const { validationResult } = require('express-validator/check');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
@@ -30,14 +32,20 @@ exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
     // create post in db
-    res.status(201).json({
-        message: 'Post created successfully!',
-        post: {
-            _id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: { name: 'Steve' },
-            createdAt: new Date()
-        }
+    const post = new Post({
+        title: title,
+        content: content,
+        creator: { name: 'Steve' },
+    });
+    post.save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Post created successfully!',
+            post: result
+        });
+    })
+    .catch(err => {
+        console.log(err);
     });
 };
